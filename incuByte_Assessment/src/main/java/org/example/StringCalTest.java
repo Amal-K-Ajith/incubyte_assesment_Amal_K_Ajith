5d9b20c8aa706727cfe9aafe15fcaf5b0d169d76
Amal-K-Ajith <amalajithk10@gmail.com>
2023-05-21 13:15:51 +0530

    delimiter support added and passed test

--- incuByte_Assessment/src/main/java/org/example/StringCalTest.java
+++ incuByte_Assessment/src/main/java/org/example/StringCalTest.java
@@ -1,25 +1,24 @@
 package org.example;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 
 public class StringCalTest {
     StringCalculator sc=new StringCalculator();
     @Test
     public void test1() throws Exception {
-        assertEquals(sc.add("1 1"), 2);
-        assertEquals(sc.add("3,4"), 7);
-        assertEquals(sc.add("3,4.5"), 12);
-        assertEquals(sc.add("12 1"),13);
+        assertEquals(2,sc.add("1 1"));
+        assertEquals(7,sc.add("3,4"));
+        assertEquals(12,sc.add("3,4,5"));
+        assertEquals(13,sc.add("12 1"));
         NegativeNotAllowedException thrown = Assertions.assertThrows(NegativeNotAllowedException.class, () -> {
             sc.add("3,-4");
         });
-        Assertions.assertEquals("Negative Not Allowed", thrown.getMessage());
-        assertEquals(sc.add("10\n 12"),22);
-        assertEquals(sc.add("//;\n 1;2"),3);
+        assertEquals("Negative Not Allowed", thrown.getMessage());
+        assertEquals(25,sc.add("10\n 12 \n 3"));
+        assertEquals(20,sc.add("10\n 6,4"));
+        assertEquals(3,sc.add("//;\n 1;2"));
+        assertEquals(32,sc.add("//:\n10:12:10"));
     }
 }
--- incuByte_Assessment/src/main/java/org/example/StringCalculator.java
+++ incuByte_Assessment/src/main/java/org/example/StringCalculator.java
@@ -1,18 +1,22 @@
 package org.example;
 
-
-
 public class StringCalculator {
 
     int add(String st) throws Exception {
-        st=st.replaceAll("\n",",");
-        String [] ch =st.split("[,. \n]");
-        int sum=0;
-        for(String s:ch){
-            if(s=="")continue;
-            int num=Integer.parseInt(s);
-            if(num<0)throw new NegativeNotAllowedException();
-            sum=sum+num;
+        char deLimiter = ';';
+        int d_ind = st.indexOf("//");
+
+        if (d_ind != -1) {
+            st = st.replaceAll(st.charAt(d_ind + 2) + "", ",").substring(4);
+        }
+        st = st.replaceAll("\n", ",");
+        String[] ch = st.split("[,. ]");
+        int sum = 0;
+        for (String s : ch) {
+            if (s == "") continue;
+            int num = Integer.parseInt(s);
+            if (num < 0) throw new NegativeNotAllowedException();
+            sum = sum + num;
         }
         System.out.println();
         return sum;
